ebf460c62bc1230dbbd439ea2148193a07a1072c
Kostiantyn Kalynovskyi <kkalynovskyi@mirantis.com>
2019-11-06 09:44:10 -0600

    [AIR-97] Add kubectl apply wrapper package
    The wrapper is called ApplyAdapter and is a struct, that has Apply(..)
    method and some setters that allow to control kubectl apply behaviour
    
    Addapter is expected to be used through Apply(..) function, which takes
    slice of document.Document interface objects, writes them out to
    temporary file system, from where they are picked up by kubectl Apply
    module, and delivered to kubernetes cluster. The decision to use
    temporary file system is based on the fact, that in current state
    kubectl project currently only works with actual files, and ignores
    io.Streams object, that is part of ApplyOptions struct, so it is
    currently the only way to use it.
    
    Change-Id: Idc5d79794149c00198f420d76cf9aa3b5264946e

---
+++ pkg/k8s/filesystem.go
@@ -0,0 +1,27 @@
+package k8s
+
+import (
+	"io/ioutil"
+
+	"sigs.k8s.io/kustomize/v3/pkg/fs"
+)
+
+// File extends kustomize File and provide abstraction to creating temporary files
+type File interface {
+	fs.File
+	Name() string
+}
+
+// FileSystem extends kustomize FileSystem and provide abstraction to creating temporary files
+type FileSystem interface {
+	fs.FileSystem
+	TempFile(string) (File, error)
+}
+
+// Buffer is adaptor to TempFile
+type Buffer struct {
+	fs.FileSystem
+}
+
+// TempFile creates file in temporary filesystem, at default os.TempDir
+func (b Buffer) TempFile(prefix string) (File, error) { return ioutil.TempFile("", prefix) }
