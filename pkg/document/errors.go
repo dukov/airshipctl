2fd04ade469c495153d013455884d5e350516176
Aleksey Deryugin <aderiugin@mirantis.com>
2019-12-27 14:07:35 +0400

    Filter resources by label instead of annotation
    Use labels to select resources from document model

--- pkg/document/errors.go
+++ pkg/document/errors.go
@@ -6,10 +6,10 @@ import (
 
 // ErrDocNotFound returned if desired document not found
 type ErrDocNotFound struct {
-	Annotation string
-	Kind       string
+	Label string
+	Kind  string
 }
 
 func (e ErrDocNotFound) Error() string {
-	return fmt.Sprintf("Document annotated by %s with Kind %s not found", e.Annotation, e.Kind)
+	return fmt.Sprintf("Document labeled by %s with Kind %s not found", e.Label, e.Kind)
 }
